83a5bd8ebd405105cfc69a2418883527504b6e10
Tiago Alba Moreno <tiagoalba@hotmail.com>
2022-03-08 23:50:24 -0300

    Criei branch para exercicios lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,52 @@
+/*  Exercicio 1
+ele pede pra buscar os numeros abaixo de 5 e em seguida adiciona 
+valor e soma todos os encontrados 
+Exercico 2
+A.ele vai procurar dentro do array  todos os numeros maiores que 18 
+B. sim so coloar  numero > 0 ele vai procurar por todos indices maiores que 0
+
+Exercico 3
+lingas de *  na quantidade pedida no caso 4 ele iria rodar ate aparecer 4 *
+
+
+*/
+// Escrita de codigo
+// Exercicio 1
+/*let nomesPet = []
+const pets = +prompt("Você tem quantos pets?")
+let pet = 0
+if(pet === pets){
+    console.log ("Que pena! Você pode adotar um pet!") 
+}else{
+    
+    
+ for (let i = 0; i < pets ; i++) {
+    if( i < pets){
+         let novoPet = prompt("qual seu pet")
+            nomesPet.push(novoPet)
+        }
+    }
+ console.log(nomesPet)
+ nomesPet++
+    }*/
+    //Exercicio 2 
+    const arrayOriginal = [25 , 30 , 50 , 60 , 70]
+//A.
+/*{
+    for (const array of arrayOriginal) {
+       console.log( array)
+    }
+}*/
+//B.
+
+/*for (const numero of arrayOriginal) {
+    let divisao = numero / 10
+    console.log(divisao)
+
+}   */
+ //const novoArray = []
+ //for (const number of arrayOriginal) {
+   //  let par = number % 2 === 0 
+     //novoArray.push(par)
+ //}
+ //console.log(novoArray)
