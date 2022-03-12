e2400f29c36755514fefd4057989d177812022bf
Tiago Alba Moreno <tiagoalba@hotmail.com>
2022-03-12 00:03:14 -0300

    Criei atrasados uns minutos a branch black jack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,48 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+/*function somaDuasCartas(player) {
+
+   let number1 = comprarCarta()
+   let number2 = comprarCarta()  
+   let soma = number1.valor + number2.valor
+   let card = number1.texto
+   let card2 = number2.texto 
+   
+   console.log(player,"carta -", card , card2 ,"Pontuação -", soma)
+   return soma 
+}
+
+console.log("Boas vindas ao BlackJack!")
+const start = confirm("Quer iniciar uma nova rodada?")
+
+if (start){
+   
+      const usuario = somaDuasCartas("Usuário")
+      const computador = somaDuasCartas("Computador")
+
+   
+         if(usuario > computador){
+         console.log("O Usuario ganhou!")
+      
+             }else if( usuario === computador){
+               console.log("Empate!")
+      
+               }else{
+               console.log("O Computador ganhou!")
+               }
+               
+}else { 
+console.log("O Jogo Acabou!")
+}
+*/
+   
