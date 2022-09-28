4103e6ce792b1eaa0596df78a732e87c0693db56
Tiago Alba Moreno <tiagoalba@hotmail.com>
2022-09-27 22:30:01 -0300

    branch exercicio classe encapsulamento

---
+++ modulo5/classes-encapsulamento/src/models/User.ts
@@ -0,0 +1,25 @@
+/*export type User = {
+    id: string,
+    email: string,
+    password: string
+}*/
+export class User {
+constructor(
+    private id : string,
+    private email: string,
+    private password: string
+){
+    this.id = id
+    this.email = email
+    this.password = password
+}
+getId(): string{
+    return this.id
+}
+getEmail(): string{
+    return this.email
+}
+getPassword(): string{
+    return this.password
+}
+}
